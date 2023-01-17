4274751485b10cf2fe4d55314aba36dd46a5c50a
Emilia <emilia.rus@polymtl.ca>
2023-01-17 13:00:06 -0500

    consignes NombresPremiers

---
+++ NombresPremiers.cpp
@@ -0,0 +1,10 @@
+// Nombres premiers : Écrivez un programme qui lit du clavier un nombre entier, puis détermine si ce
+// nombre est premier ou non. S’il n’est pas premier, le plus petit entier qui est un diviseur de ce nombre est
+// affiché. Pour trouver un diviseur, commencez par vérifier si le nombre entré est divisible par 2, sinon
+// essayez de le diviser par les nombres impairs entre 3 et √, et arrêtez-vous au plus petit (aide : un
+// algorithme similaire est écrit dans l’énoncé du TD1).
+// Exemple d’affichage :
+// Entrer un nombre entier : 8
+// Ce nombre n'est pas premier car il est divisible par 2.
+// Entrer un nombre : 2
+// Ce nombre est premier. 
