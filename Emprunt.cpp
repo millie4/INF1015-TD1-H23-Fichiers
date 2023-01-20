54f5c619e208cecf1cfde28727a277796cfea619
Emilia Rus <emilia.rus@polymtl.ca>
2023-01-19 21:45:48 -0500

    correction des normes de codage fichiers Emprunt et NombresPremiers

--- Emprunt.cpp
+++ Emprunt.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
+#define INFINITY __builtin_inff();
+#include <cmath>
 
 
-bool validation(int number, int borneSup, int borneInf, int montant) {
+bool validerBornes(int nombre, int borneSup, int borneInf) {
         bool valide = true;
-        if (number > borneSup || number < borneInf || montant < 0 ) {
+        if (nombre > borneSup || nombre < borneInf) {
             valide = false;
         }
         return valide;
@@ -12,34 +14,34 @@ bool validation(int number, int borneSup, int borneInf, int montant) {
 int main() {
     while (true) {
 
-        double somme_argent;
-        double montant_mensuel;
-        double taux_interet;
+        double sommeArgent;
+        double montantMensuel;
+        double tauxInteret;
 
         cout << "Entrez la somme d'argent pretee : ";
-        cin >> somme_argent;
+        cin >> sommeArgent;
         cout << "Entrez le montant rembourse mensuellement : "; 
-        cin >> montant_mensuel;
+        cin >> montantMensuel;
         cout << "Entrez le taux d'interet annuel : ";
-        cin >> taux_interet;
+        cin >> tauxInteret;
 
         // calcul du taux d'interets mensuel
-        double taux_mensuel = taux_interet / 1200.0;
+        double tauxMensuel = tauxInteret / 1200.0;
 
-        // verification de la validité des données
-        if (validation(taux_interet, 100.0, 0.0, somme_argent)) {
+        // verification des bornes des donnees
+        if (validerBornes(tauxInteret, 100.0, 0.0) & validerBornes(sommeArgent, HUGE_VAL, 0.0)) {
 
-            // Calcul du montant total à rembourser, interets inclues 
-            double MontantArembourse = somme_argent * (1.0 + taux_interet/100.0);
-            // Calcul du montant remboursé mensuellement, intérets inclues 
-            double Mensualite = montant_mensuel * (1.0 + taux_mensuel);
-            // Calcul du nombre de mois nécessaires pour le remboursement
-            double NombreDeMois = MontantArembourse / Mensualite;
+            // Calcul du montant total a rembourser, interets inclus 
+            double dette = sommeArgent * (1.0 + tauxInteret/100.0);
+            // Calcul du montant rembourse mensuellement, interets inclus 
+            double mensualite = montantMensuel * (1.0 + tauxMensuel);
+            // Calcul du nombre de mois necessaires pour le remboursement
+            double nombreDeMois = dette / mensualite;
             // Calcul des interets totaux percus par le pretteur
-            double InteretsTotaux = montant_mensuel * taux_mensuel * NombreDeMois;
+            double interetsTotaux = montantMensuel * tauxMensuel * nombreDeMois;
 
-            cout << NombreDeMois << endl;
-            cout << InteretsTotaux << endl;
+            cout << nombreDeMois << endl;
+            cout << interetsTotaux << endl;
             break;
 
         }
--- NombresPremiers.cpp
+++ NombresPremiers.cpp
@@ -1,14 +1,3 @@
-// Nombres premiers : Écrivez un programme qui lit du clavier un nombre entier, puis détermine si ce
-// nombre est premier ou non. S’il n’est pas premier, le plus petit entier qui est un diviseur de ce nombre est
-// affiché. Pour trouver un diviseur, commencez par vérifier si le nombre entré est divisible par 2, sinon
-// essayez de le diviser par les nombres impairs entre 3 et √, et arrêtez-vous au plus petit (aide : un
-// algorithme similaire est écrit dans l’énoncé du TD1).
-// Exemple d’affichage :
-// Entrer un nombre entier : 8
-// Ce nombre n'est pas premier car il est divisible par 2.
-// Entrer un nombre : 2
-// Ce nombre est premier. 
-
 #include <iostream>
 using namespace std;
 #include <cmath>
@@ -17,25 +6,25 @@ int main () {
 
     int i; 
     int nombre; // notre nombre entier 
-    bool is_prime = true;
+    bool est_prime = true;
     cout << "Entrez un nombre entier : ";
     cin >> nombre;
 
     // O et 1 sont des nombres premiers 
     if (nombre == 0 || nombre == 1) {
-        is_prime = false;
+        est_prime = false;
     }
 
     // Boucle pour verifier si le nombre est premier 
     for (i = 2; i <= nombre/2; ++i) {
         if (nombre % i == 0) {
-        is_prime = false;
+        est_prime = false;
         break;
         }
     }
 
     int n = 3;
-    if (is_prime)
+    if (est_prime)
         cout << nombre << " est un nombre premier";
     else {
         if (nombre % 2 == 0) {
