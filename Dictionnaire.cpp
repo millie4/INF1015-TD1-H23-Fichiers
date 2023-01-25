22d58346b550339b495886eefab2e96ea9549d36
Emilia Rus <emilia.rus@polymtl.ca>
2023-01-25 12:16:05 -0500

    fichier texte dictionnaire

---
+++ Dictionnaire.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <random>
+#define HUGE_VAL __builtin_huge_val();
+#include <string>
+using namespace std;
+
+
+int main () {
+    int nbrMots = 0;
+    str nomDictionnaire = dictionnaire.txt;
+
+}
