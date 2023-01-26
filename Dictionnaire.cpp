7996aaf2f0cc077e8633c1597c4f82818c0b7e98
Lyna028 <Kettanilyna@gmail.com>
2023-01-26 15:18:22 -0500

    exo6

--- Dictionnaire.cpp
+++ Dictionnaire.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
-#include <cmath>
-#include <random>
 #define HUGE_VAL __builtin_huge_val();
 #include <string>
+#include <fstream>
 using namespace std;
 
 
 int main () {
-    int nbrMots = 0;
-    str nomDictionnaire = dictionnaire.txt;
+    int nbrMots = 4;
+    int tableau[nbrMots];
 
+    ifstream file("dictionnaire.txt");
+    int count; // the number of lines
+    string* lines = nullptr; // array of strings
+
+    if(file.is_open()) {
+        string myArray[5];
+
+        for(int i = 0; i < 5; ++i)
+        {
+            file >> myArray[i];
+        }
+    }
 }
